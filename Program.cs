97d17c4bc08cc7baa7c617f1edd05d6a4c679710
marianozzz <mariano.zanier@gmail.com>
2021-06-13 18:48:17 -0300

    inital commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace school.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(1, "Pepe", "Grillo", "miemail@gmail.com", "123466", "20-06-2021", "20-06-2021");
+
+            Console.WriteLine(person.ToString());
+            Console.WriteLine(person.Name+", "+person.Lastname);
+
+            
+            Console.Read();
+        }
+    }
+}
